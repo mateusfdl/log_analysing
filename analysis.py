6b8d91636cc10f5fe617694c6b2bcbd310406d0f
Matheus Lima <matheus.limastack@outlook.com>
2021-09-14 11:09:32 -0300

    Add data_frame as method

--- analysis.py
+++ analysis.py
@@ -10,30 +10,31 @@ class Analysis:
 
     def export(self):
         self.append_parsed_data()
-        data_frame = pandas.DataFrame(self.data, columns=['authenticated_entity', 'latencies', 'route'])
-        self.export_average_latencie(data_frame)
-        self.export_request_per_consumer(data_frame)
-        self.export_request_per_service(data_frame)
+        self.export_average_latencie()
+        self.export_request_per_consumer()
+        self.export_request_per_service()
 
     def append_parsed_data(self):
         for line in self.lines:
             self.data.append(json.loads(line))
+    
+    def data_frame(self):
+        return pandas.DataFrame(self.data, columns=['authenticated_entity', 'latencies', 'route'])
 
-
-    def export_average_latencie(self, data_frame):
+    def export_average_latencie(self):
         avg_latencie = pandas.concat([
-            pandas.json_normalize(data_frame['latencies']), 
-            data_frame.route.apply(lambda route: route['service']['id'])
+            pandas.json_normalize(self.data_frame()['latencies']), 
+            self.data_frame().route.apply(lambda route: route['service']['id'])
             .apply(pandas.Series)], axis=1).groupby(0)[['kong', 'request', 'proxy']].agg(['mean'])
 
         avg_latencie.to_csv(r'./logs/avg_latencies.csv', sep='\t', encoding='utf-8')
 
-    def export_request_per_service(self, data_frame):
-        data_frame['route'].apply(lambda route: 
+    def export_request_per_service(self):
+        self.data_frame()['route'].apply(lambda route: 
                 route['service']['id']).value_counts().to_csv(
                         r'./logs/request_per_service.csv', sep='\t', encoding='utf-8')
 
-    def export_request_per_consumer(self, data_frame):
-        data_frame['authenticated_entity'].apply(lambda authenticated_entity: 
+    def export_request_per_consumer(self):
+       self.data_frame()['authenticated_entity'].apply(lambda authenticated_entity: 
                 authenticated_entity['consumer_id']['uuid']).value_counts().to_csv(
                         r'./logs/request_per_consumer.csv', sep='\t', encoding='utf-8')
